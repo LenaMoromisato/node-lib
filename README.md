611b80669abb1bc83fdf682f67c4ccde217d1609
LenaMoromisato <moromisato.lena@gmail.com>
2023-05-10 15:15:41 -0300

    criando um projeto em node

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 > Status do projeto: em desenvolvimento
 
 Conteúdo do curso:
+
+* Para criar um projeto em Node.js utilizamos o comando **npm init**;
+* O arquivo package.json é utilizado pelo Node.js para listar as dependências instaladas, além de informações sobre versão do programa, autoria e scripts;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('olá mundo');
+// terminal: npm install chalk@5.0.1 --save-exact
+
+import chalk from 'chalk';
+
+console.log(chalk.blue('olá mundo'));
+
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
