3a7c8bcd1af7e01ae1464fbf5a465bb8bce49cf6
LenaMoromisato <moromisato.lena@gmail.com>
2023-05-11 18:57:10 -0300

    capturando grupos

--- index.js
+++ index.js
@@ -3,6 +3,17 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = regex.exec(texto);
+  console.log(capturas);
+
+}
+
+extraiLinks(textoTeste);
+
 function trataErro(erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
@@ -19,29 +30,4 @@ async function pegaArquivo(caminhoDoArquivo) {
     }
 } 
 
-/* finally ----- 
-async function pegaArquivo(caminhoDoArquivo) {
- const encoding = 'utf-8';
- try {
-   const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-   console.log(chalk.green(texto))
- } catch(erro) {
-   trataErro(erro);
- } finally {
-   console.log(chalk.yellow('operação concluída'));
- }
-}
-*/
-
-/* promisses com then() -----
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.promises
-        .readFile(caminhoDoArquivo, encoding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch(trataErro)
-}
-*/
-
-pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
+// pegaArquivo('./arquivos/texto.md');
