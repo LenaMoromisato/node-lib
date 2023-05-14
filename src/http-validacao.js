2279ff1647055197aaece7f107387d66cfff02e8
LenaMoromisato <moromisato.lena@gmail.com>
2023-05-13 22:57:38 -0300

    montando o objeto

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function extraiLinks (arrLinks) {
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
@@ -5,23 +7,32 @@ function extraiLinks (arrLinks) {
 async function checaStatus (listaURLs) {
     const arrStatus = await Promise.all(
         listaURLs.map(async (url) => {
-            const response = await fetch(url);
-            return response.status;
+            try {
+                const response = await fetch(url);
+                return response.status;
+            } catch (erro) {
+                return manejaErros(erro);
+            }
         })
     )
     return arrStatus;
 }
 
+function manejaErros (erro) {
+    if (erro.cause.code === 'ENOTFOUND') {
+        return 'link não encontrado';
+    } else {
+        return 'ocorreu algum erro';
+    }
+}
+
 export default async function listaValidada (listaDeLinks) {
     const links = extraiLinks(listaDeLinks);
     const status = await checaStatus(links);
-    return status;
+
+    return listaDeLinks.map((objeto, indice) => ({
+        ...objeto,
+        status: status[indice]
+    }))
 }
 
-// const res = await fetch('https://nodejs.org/api/documentation.json');
-// if (res.ok) {
-//     const data = await res.json();
-//     console.log(data);
-// }
-
-//[gatinho salsicha](http://gatinhosalsicha.com.br/)
